175b50e16187bbc08bdaf4081b1fd043d9092ec8
mindc1may62 <utsav.gautam@mindtree.com>
2022-06-27 13:20:40 +0530

    added

---
+++ ArtitecturePortfolioApp/Controllers/EnquiriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ArtitecturePortfolioApp.Models;
+
+namespace ArtitecturePortfolioApp.Controllers
+{
+    public class EnquiriesController : Controller
+    {
+        private ArtitecturePortfolio_DBEntities db = new ArtitecturePortfolio_DBEntities();
+
+        // GET: Enquiries
+        public ActionResult Index()
+        {
+            return View(db.Enquiries.ToList());
+        }
+
+        // GET: Enquiries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enquiry enquiry = db.Enquiries.Find(id);
+            if (enquiry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enquiry);
+        }
+
+        // GET: Enquiries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enquiries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,MobileNumber,Address,Email,Query")] Enquiry enquiry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enquiries.Add(enquiry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(enquiry);
+        }
+
+        // GET: Enquiries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enquiry enquiry = db.Enquiries.Find(id);
+            if (enquiry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enquiry);
+        }
+
+        // POST: Enquiries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,MobileNumber,Address,Email,Query")] Enquiry enquiry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enquiry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(enquiry);
+        }
+
+        // GET: Enquiries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enquiry enquiry = db.Enquiries.Find(id);
+            if (enquiry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enquiry);
+        }
+
+        // POST: Enquiries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enquiry enquiry = db.Enquiries.Find(id);
+            db.Enquiries.Remove(enquiry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ArtitecturePortfolioApp/Models/MetaClasses/EnquiryMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArtitecturePortfolioApp.Models
+{
+    public class EnquiryMetaData
+    {
+        [StringLength(20)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        [StringLength(100)]
+        [Required]
+        public string Query { get; set; }
+
+    }
+    [MetadataType(typeof(EnquiryMetaData))]
+    public partial class Enquiry { }
+}
